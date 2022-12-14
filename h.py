d64c740bc07ae03573f298cc4f777af1253300f1
Raunak Palewar <78296962+raunakpalewar@users.noreply.github.com>
2022-12-15 01:37:29 +0530

    Update h.py

--- h.py
+++ h.py
@@ -36,47 +36,20 @@ for row in range(0, dataframe1.max_row):
             img= soup.find("img")
             
             img=img.find_next('img')
-            # fig, ax = plt.subplots()
-            # fig.savefig('myimage.svg', format='svg', dpi=1200)
+           
             print(img['src'])  
             
-            # SAVE_FOLDER = 'imagess'
-            # os.mkdir(SAVE_FOLDER)
-            # width,height = 'some_w', 'some_h'
-            # req = io.imread(img['src'])
+           
 
 
             try:
                 urllib.request.urlretrieve(img['src'], word+"_img_.png")
-            # cv2.imwrite(img['src'], word+"_img_.png", cv2.resize(req, (width,height)))
             except:
                 e=open("error.txt","a")
                 e.write(word+"\n")
 
                 print("Error\n")
 
-            # dimensions = (500, 500)
-            # q = Image.open(word+"_img_.png")
-            # q.thumbnail(dimensions)
-            # q.show()
-            # try:
-            #     p = wikipedia.page(word,auto_suggest=False)
-            # except wikipedia.exceptions.DisambiguationError as e:
-            #     break
-            # except wikipedia.exceptions.PageError as f:
-            #     break
-            # print(p.url)
-            # print(p.title)
-            # content = p.content # Content of page.
-            # x=0
-            # text=""
-            # for j in content:
-            #     if x<2:
-            #         text+=j
-            #         if(j=='.'):
-            #             x+=1
-            # with open(word+"_text.txt","w") as f:
-            #     f.write(text)
 
             i=i+1             
             if(i>=1):
